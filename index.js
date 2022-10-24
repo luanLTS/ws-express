8afefd9d3f9bb94c82b38becbc9a268e36cf17c5
Luan Teixeira <luanlts@outlook.com>
2022-10-24 12:15:57 -0300

    feat: implementa metodo de requisicao com o servidor websocket

--- index.js
+++ index.js
@@ -13,8 +13,40 @@ const wss = new WebSocketServer({
     autoAcceptConnections: false,
 });
 
+let nextIdConn = 1;
+const connectionsArray = [];
+
+const allowClient = () => {
+    // logica para liberar a conexao ou não
+    return true;
+}
 
 wss.on('request', (request) => {
     console.log(request);
+    console.log(request.origin);
+    if (!allowClient()) {
+        request.reject(401);
+        return;
+    }
 
+    // ao aceitar a conexão é devolvido o objeto connection
+    let connection = request.accept();
+
+    // adiciona a conexão ao vetor de conexões ativas
+    connectionsArray.push(connection);
+
+    // define um id para a conexão e incrementa para a proxima
+    connection.ID = nextIdConn;
+    nextIdConn++;
+
+    var message = {
+        type: "id",
+        id: connection.ID,
+    }
+    // envia ao cliente o id da sua conexao com o servidor
+    connection.sendUTF(JSON.stringify(message));
+
+    connection.on('message', (data) => {
+        //tratar as mensagens vinda do cliente
+    })
 });
